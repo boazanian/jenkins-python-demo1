87c4889b6359b2d25dc936ec71224038f906e74f
boazanian <46721553+boazanian@users.noreply.github.com>
2023-04-11 12:40:06 -0400

    Inupdate ko. M mydemo.py

--- mydemo.py
+++ mydemo.py
@@ -1,1 +1,26 @@
+# To setup a token (as password) , go to Profile > Settings > Developer settings > Personal access tokens > Token  (classic) > Generate new token and select Generate new token (classic).
+# Check box the permissions
+# Generate a new Personal Access Token (also classic). Make sure you copy the Personal Access Token as soon as it gets generated — you won’t be able to see it again!
+
+
+# Sample project
+# mkdir jenkins-python-demo1
+# cd hjenkins-python-demo1
+#
+# git init
+# This command creates a hidden folder called .git/ which contains the configuration for our repository. 
+# 
+# git remote add origin https://github.com/boazanian/jenkins-python-demo1.git
+#
+# git add .
+
+# git push -u origin master
+# Let’s see what happens when we run this command
+# error: src refspec master does not match any.
+#
+# git commit -m "Add mydemo.py"
+
+error: src refspec master does not match any.
+
 print("Hello World")
+print("at sa Universe")
